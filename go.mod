0f137921c86ddec037e95e6634bb8fd908ec0e91
letenk <darmawanrizky43@gmail.com>
2024-08-27 22:07:57 +0700

    ref: simulate without real resource

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/letenk/thundering_herd_problem
 go 1.21.3
 
 require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-sql-driver/mysql v1.8.1
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.34.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"fmt"
-	"github.com/go-redis/redis"
-	_ "github.com/go-sql-driver/mysql"
 	"log"
-	"math/rand"
 	"time"
 )
 
@@ -18,156 +13,115 @@ type User struct {
 }
 
 type Config struct {
-	redisClient *redis.Client
-	dbClient    *sql.DB
-	circuit     *CircuitBreaker
+	circuit *CircuitBreaker
 }
 
 type CircuitBreaker struct {
 	failureThreshold   int
+	successThreshold   int
 	consecutiveFailure int
+	successCount       int
 	open               bool
+	halfOpen           bool
 	openedAt           time.Time
 }
 
-const circuitBreakerResetTimeout = 2 * time.Second
+const (
+	circuitBreakerResetTimeout = 2 * time.Second
+	successThreshold           = 5
+)
 
 func NewCircuitBreaker(failureThreshold int) *CircuitBreaker {
 	return &CircuitBreaker{
 		failureThreshold:   failureThreshold,
+		successThreshold:   successThreshold,
 		consecutiveFailure: 0,
+		successCount:       0,
 		open:               false,
+		halfOpen:           false,
 	}
 }
 
-func NewUser(dbClient *sql.DB, redisClient *redis.Client, failureThreshold int) *Config {
+func NewConfig(failureThreshold int) *Config {
 	return &Config{
-		dbClient:    dbClient,
-		redisClient: redisClient,
-		circuit:     NewCircuitBreaker(failureThreshold),
+		circuit: NewCircuitBreaker(failureThreshold),
 	}
 }
 
-func (cb *CircuitBreaker) IsOpen() bool {
-	if cb.open {
-		// Check last time opened
-		if time.Since(cb.openedAt) >= circuitBreakerResetTimeout {
-			cb.open = false
-			cb.consecutiveFailure = 0
-			log.Println("Circuit Breaker closed")
-		} else {
-			return true
-		}
+func (cb *CircuitBreaker) AllowRequest() bool {
+	if !cb.open {
+		return true
+	}
+
+	if time.Since(cb.openedAt) >= circuitBreakerResetTimeout {
+		cb.halfOpen = true
+		log.Println("Circuit Breaker half-open")
+		return true
 	}
 	return false
 }
 
-func (cb *CircuitBreaker) IncrementConsecutiveFailure() {
+func (cb *CircuitBreaker) RecordSuccess() {
+	cb.consecutiveFailure = 0
+	if cb.halfOpen {
+
+		cb.successCount++
+
+		if cb.successCount >= cb.successThreshold {
+			cb.open = false
+			cb.halfOpen = false
+			cb.successCount = 0
+			log.Println("Circuit Breaker closed")
+		}
+	}
+}
+
+func (cb *CircuitBreaker) RecordFailure() {
 	cb.consecutiveFailure++
-	if cb.consecutiveFailure >= cb.failureThreshold {
+	cb.successCount = 0
+	if cb.halfOpen || cb.consecutiveFailure >= cb.failureThreshold {
 		cb.open = true
+		cb.halfOpen = false
 		cb.openedAt = time.Now()
 		log.Println("Circuit Breaker opened")
 	}
 }
 
-func (e *Config) getDataFromMysql(username string) (*User, error) {
-	row := e.dbClient.QueryRow("SELECT * FROM users WHERE username = ?", username)
-
-	user := &User{}
-	err := row.Scan(&user.Id, &user.Username, &user.Email)
-	if err != nil {
-		return nil, err
-	}
-
-	// Save to redis
-	err = e.saveToRedis(username, user)
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
-
-}
-
-func (e *Config) getDataFromRedis(username string) (*User, error) {
-	if e.circuit.IsOpen() {
+func (c *Config) simulateDataFetch(username string, requestNumber int) (*User, error) {
+	if !c.circuit.AllowRequest() {
 		return nil, fmt.Errorf("circuit breaker is open")
 	}
 
-	val, err := e.redisClient.Get(username).Result()
-	if err != nil {
-		log.Printf("failed to get redis with key [%s], err: %v", username, err)
+	time.Sleep(50 * time.Millisecond)
 
-		log.Println("get data from mysql")
-		user, err := e.getDataFromMysql(username)
-		if err != nil {
-			e.circuit.IncrementConsecutiveFailure()
-			return nil, err
-		}
-
-		return user, nil
-
+	if requestNumber >= 5 && requestNumber <= 10 {
+		c.circuit.RecordFailure()
+		return nil, fmt.Errorf("simulated failure")
 	}
 
-	user := &User{}
-	err = json.Unmarshal([]byte(val), &user)
-	if err != nil {
-		return nil, err
+	user := &User{
+		Id:       requestNumber,
+		Username: username,
+		Email:    fmt.Sprintf("%s@example.com", username),
 	}
 
+	c.circuit.RecordSuccess()
 	return user, nil
 }
 
-func (e *Config) saveToRedis(key string, data *User) error {
-	// Convert data to JSON
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
-
-	// Save to redis
-	ttl := 2 * time.Millisecond
-	err = e.redisClient.Set(key, jsonData, ttl).Err()
-	if err != nil {
-		return err
-	}
-
-	return nil
-
-}
-
 func main() {
-	mysqlConn, err := sql.Open("mysql", "root:mysqlsecret@tcp(localhost:3306)/employees")
-	if err != nil {
-		panic(err)
-	}
-
-	redisConn := redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
-		Password: "",
-		DB:       0,
-	})
-
 	failureThreshold := 3
-	client := NewUser(mysqlConn, redisConn, failureThreshold)
+	client := NewConfig(failureThreshold)
 
 	username := "user20001"
 
-	// Simulate get cache miss as asyncronous
-	for i := 0; i < 100; i++ {
-		go func() {
-			time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-			val, err := client.getDataFromRedis(username)
-			if err != nil {
-				fmt.Println(err)
-
-			} else {
-				fmt.Printf("Got value: %v\n", val)
-
-			}
-		}()
+	for i := 1; i <= 80; i++ {
+		val, err := client.simulateDataFetch(username, i)
+		if err != nil {
+			fmt.Printf("Request %d: %v\n", i, err)
+		} else {
+			fmt.Printf("Request %d: Got value: %v\n", i, val)
+		}
+		time.Sleep(100 * time.Millisecond) // Add delay between requests
 	}
-
-	select {}
 }
